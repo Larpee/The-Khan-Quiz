890b01f91c72aabc0c0e4c4673aa964ad3996001
Larpee <pilar.lopezmaggi@gmail.com>
2017-03-01 14:18:18 -0300

    Create insertGame.js

---
+++ insertGame.js
@@ -0,0 +1,13 @@
+$.get("https://raw.githubusercontent.com/Larpee/The-Khan-Quiz/master/game.js", function (game) {
+    var sketchProc = function (processingInstance) {
+        with (processingInstance) {
+            size(400, 400);
+            frameRate(30);
+            eval(game);
+        }
+    };
+    
+    var canvas = document.getElementById("game");
+
+    var processingInstance = new Processing(canvas, sketchProc);
+  });
